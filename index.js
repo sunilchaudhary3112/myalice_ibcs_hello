51cac9552c53219e5653e9c332d1df625162d514
sunilchaudhary3112 <sunilchaudhary3112@gmail.com>
2018-08-13 12:19:52 +0530

    added correct port for heroku

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 var app = require('./app.js');
 
 //set parameters as appropriate
-var port = 4001;
+var port = process.env.PORT || 4001;
 const config = {
-    root: __dirname,
-    port: process.env.PORT || 4001,
-    logLevel: 'INFO',
-    logger: null,
-		basicAuth: null,
-		sslOptions: null
+  root: __dirname,
+  port: process.env.PORT || 4001,
+  logLevel: 'INFO',
+  logger: null,
+  basicAuth: null,
+  sslOptions: null
 };
 
 console.log(config);
@@ -18,6 +18,6 @@ console.log(config);
 var express_app = app.init(config);
 
 // Start the server listening..
-express_app.listen(port,()=>{
+express_app.listen(port, () => {
   console.log("Server running at port" + port);
 });
